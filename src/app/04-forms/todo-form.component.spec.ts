14c7162592cb8e9eaa4d4f574a597796565602ce
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-02-12 16:33:20 +0000

    testing forms

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,23 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { TodoFormComponent } from './todo-form.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  let component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should createa a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBe(true);
+    expect(component.form.contains('email')).toBe(true);
   });
 
-  it('', () => {
+  it('should make the name control required', () => {
+    const control = component.form.get('name');
+
+    control.setValue('');
+
+    expect(control.valid).toBe(false);
   });
 });
--- src/app/04-forms/todo-form.component.ts
+++ src/app/04-forms/todo-form.component.ts
@@ -1,13 +1,15 @@
 
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
-export class TodoFormComponent { 
-  form: FormGroup; 
+export class TodoFormComponent {
+  form: FormGroup;
 
+  // Form builder object being used to create a form group
+  // with 2 form controls
   constructor(fb: FormBuilder) {
     this.form = fb.group({
       name: ['', Validators.required],
       email: [''],
     });
   }
-}
+};
