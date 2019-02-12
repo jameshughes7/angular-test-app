623c9ed8bcdc2e82d5e6ad44beaf60497819a08a
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-02-12 12:25:58 +0000

    setup and tear down

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,25 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  afterEach(() => {
+    // 'This is wear tear down instructions are given'
+  });
+
+  it('should increment totalVotes when upvoted', () => {
+    component.upVote();
+
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
   });
 });
