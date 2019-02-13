b55e82e3b18e4a97d12a6a22fe245c9a0a16ec41
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-02-13 06:46:42 +0000

    testing service

--- src/app/06-services/todos.component.ts
+++ src/app/06-services/todos.component.ts
@@ -1,26 +1,31 @@
 
-// import { TodoService } from './todo.service';
+import { TodoService } from './todo.service';
 
-// export class TodosComponent {
-//   todos: any[] = [];
-//   message;
+export class TodosComponent {
+  todos: any[] = [];
+  message;
 
-//   constructor(private service: TodoService) {}
+  constructor(private service: TodoService) {}
 
-//   ngOnInit() {
-//     this.service.getTodos().subscribe(t => this.todos = t);
-//   }
+  ngOnInit() {
+    this.service.getTodos().subscribe(t => this.todos = t);
+  }
 
-//   add() {
-//     var newTodo = { title: '... ' };
-//     this.service.add(newTodo).subscribe(
-//       t => this.todos.push(t),
-//       err => this.message = err);
-//   }
+  add() {
+    var newTodo = { title: '... ' };
+    this.service.add(newTodo).subscribe(
+      t => this.todos.push(t),
+      err => this.message = err);
+  }
 
-//   delete(id) {
-//     if (confirm('Are you sure?')) {
-//       this.service.delete(id).subscribe();
-//     }
-//   }
-// }
+  delete(id) {
+    if (confirm('Are you sure?')) {
+      this.service.delete(id).subscribe();
+    }
+  }
+}
+
+// testing ngOnInit
+// subscribe to service.gerTodos method
+// as it is returning an observable
+// initialise the todos property
