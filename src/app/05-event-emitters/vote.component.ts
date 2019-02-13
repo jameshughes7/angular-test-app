e883ac9f9092141d3c22c946f7157e855ffce8b5
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-02-13 05:52:54 +0000

    testing event emitter

--- src/app/05-event-emitters/vote.component.ts
+++ src/app/05-event-emitters/vote.component.ts
@@ -1,12 +1,17 @@
 
-import { EventEmitter } from '@angular/core'; 
+import { EventEmitter } from '@angular/core';
 
-export class VoteComponent { 
-  totalVotes = 0; 
+export class VoteComponent {
+  totalVotes = 0;
   voteChanged = new EventEmitter();
 
-  upVote() { 
+  upVote() {
     this.totalVotes++;
     this.voteChanged.emit(this.totalVotes);
   }
 }
+
+// Often we'll use an EventEmitter as an
+// Output() property
+// But won't worry about it here cos we're not using templates
+
